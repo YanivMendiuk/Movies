6377b98bb450d84f21ab8ec47026112f826ab752
Yaniv Mendiuk <yanivmendiuk@gmail.com>
2020-05-11 01:24:54 +0300

    Second commit fixed css bugs

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const express = require("express")
 const https = require("https");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express()
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-
-
-
 app.get("/", function (req, res) {
     res.render("home")
-    res.send()
 })
 
+app.get("/about", function (req, res) {
+    res.render("about")
+})
+
+app.get("/contact", function (req, res) {
+    res.render("contact")
+})
 
 app.post("/", function (req, res) {
-    console.log(req.body.movieName);
     const query = req.body.movieName;
     const url =
         "https://api.themoviedb.org/3/search/movie?api_key=f87067c77f773bfd53f4c2529a4838d8&query=" +
